fcd2deeb22123dc521cabaf94ea1fe05e0baf618
Marcos Suel Nogueira Saldanha <marcosuelns@gmail.com>
2021-03-19 11:12:20 -0300

    First Commit

---
+++ src/main/java/com/company/pratica03/Pratica04Application.java
@@ -0,0 +1,13 @@
+package com.company.pratica03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pratica04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pratica04Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/pratica03/Pratica04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.pratica03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pratica04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
