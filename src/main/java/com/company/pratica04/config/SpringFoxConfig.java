b2bac0a77f1606a76d748756d7fcade7c96a05de
Marcos Suel Nogueira Saldanha <marcosuelns@gmail.com>
2021-03-29 12:00:21 -0300

    feat: configuração das informações da API no swagger

--- src/main/java/com/company/pratica04/config/SpringFoxConfig.java
+++ src/main/java/com/company/pratica04/config/SpringFoxConfig.java
@@ -3,8 +3,11 @@ package com.company.pratica04.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -14,10 +17,22 @@ public class SpringFoxConfig {
 	@Bean
 	public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)  
-                .select()                                  
+                .select()                           
                 .apis(RequestHandlerSelectors.basePackage("com.company.pratica04"))              
-                .paths(PathSelectors.any())                          
-                .build();  
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+	}
+	
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Prática 04 API")
+				.description("*insira uma boa descrição aqui*")
+				.version("v1.0.1")
+				.contact(new Contact("Marcos Suel", "https://github.com/marcosuel", "marcosuelns@gmail.com"))
+				.license("ISC License")
+				.licenseUrl("https://opensource.org/licenses/ISC")
+				.build();
 	}
 	
 }
