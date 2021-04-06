1bba242b9c1253905b3babdd94d13d93178e3c9d
Marcos Suel Nogueira Saldanha <marcosuelns@gmail.com>
2021-04-06 11:30:52 -0300

    feat: lombok configurado.

--- src/main/java/com/company/pratica04/Pratica04Application.java
+++ src/main/java/com/company/pratica04/Pratica04Application.java
@@ -3,11 +3,17 @@ package com.company.pratica04;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.company.pratica04.dto.aluno.AlunoDto;
+
 @SpringBootApplication
 public class Pratica04Application {
 
 	public static void main(String[] args) {
+		AlunoDto dto = new AlunoDto();
+		
 		SpringApplication.run(Pratica04Application.class, args);
+		
+		
 	}
 
 }
--- src/main/java/com/company/pratica04/dto/aluno/AlunoDto.java
+++ src/main/java/com/company/pratica04/dto/aluno/AlunoDto.java
@@ -2,53 +2,16 @@ package com.company.pratica04.dto.aluno;
 
 import com.company.pratica04.dto.turma.TurmaItemAlunoDto;
 
-public class AlunoDto {
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter @Setter @EqualsAndHashCode @NoArgsConstructor
+public class AlunoDto {
 	private Long id;
 	private String nome;
 	private String sobrenome;
 	private Long matricula;
 	private TurmaItemAlunoDto turma;
-	
-	public AlunoDto() {}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Long getMatricula() {
-		return matricula;
-	}
-
-	public void setMatricula(Long matricula) {
-		this.matricula = matricula;
-	}
-
-	public TurmaItemAlunoDto getTurma() {
-		return turma;
-	}
-
-	public void setTurma(TurmaItemAlunoDto turma) {
-		this.turma = turma;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getSobrenome() {
-		return sobrenome;
-	}
-
-	public void setSobrenome(String sobrenome) {
-		this.sobrenome = sobrenome;
-	}
 }
